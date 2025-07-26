71e46f525079eb2d8839610eb788ae491efd60c6
VideoCurio <david@videocurio.com>
2025-07-26 09:19:41 +0200

    Plymouth boot splash screen

--- boot-efi.nix
+++ boot-efi.nix
@@ -19,21 +19,21 @@
       enable = true;
       theme = "breeze";
       #logo = "${pkgs.nixos-icons}/share/icons/hicolor/48x48/apps/nix-snowflake-white.png";
-
-      # Enable "Silent boot"
-      consoleLogLevel = 3;
-      initrd.verbose = false;
-      kernelParams = [
-        "quiet"
-        "splash"
-        "boot.shell_on_fail"
-        "udev.log_priority=3"
-        "rd.systemd.show_status=auto"
-      ];
-      # Hide the OS choice for bootloaders.
-      # It's still possible to open the bootloader list by pressing any key
-      # It will just not appear on screen unless a key is pressed
-      #loader.timeout = 0;
     };
+
+    # Enable "Silent boot"
+    consoleLogLevel = 3;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "udev.log_priority=3"
+      "rd.systemd.show_status=auto"
+    ];
+    # Hide the OS choice for bootloaders.
+    # It's still possible to open the bootloader list by pressing any key
+    # It will just not appear on screen unless a key is pressed
+    #loader.timeout = 0;
   };
 }
