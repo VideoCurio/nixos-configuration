df1d62550382afaf65ee9b72d1a1230da0823595
VideoCurio <david@videocurio.com>
2025-07-28 20:09:47 +0200

    Add AMD GPU configuration

---
+++ amd-gpu.nix
@@ -0,0 +1,30 @@
+# Add AMD GPU configuration
+# Must be imported by configuration.nix
+# See: https://nixos.wiki/wiki/AMD_GPU
+
+{ config, lib, pkgs, ... }:
+
+{
+  # Use the systemd-boot EFI boot loader.
+  boot = {
+    initrd.kernelModules = [ "amdgpu" ];
+    # Ban CPU integrated GPU
+    blacklistedKernelModules = [ "i915" ];
+  };
+
+  # AMD GPU
+  # Enable OpenGL
+  hardware.graphics = {
+    enable = lib.mkDefault true;
+  };
+
+  # Load driver for Xorg and Wayland
+  services.xserver.enable = lib.mkDefault true;
+  services.xserver.videoDrivers = [ "amdgpu" ];
+
+  # GUI AMD GPU controller
+  environment.systemPackages = with pkgs; [ lact ];
+  systemd.packages = with pkgs; [ lact ];
+  systemd.services.lactd.wantedBy = ["multi-user.target"];
+}
+
