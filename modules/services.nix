dc615a9b2d40cd0754258dafb82a01ea8bc2a648
VideoCurio <david@videocurio.com>
2025-08-21 14:37:16 +0200

    More xserver keyboard layout settings

--- modules/services.nix
+++ modules/services.nix
@@ -30,7 +30,7 @@
       xkb.layout = "us";
       xkb.model = "pc105";
       #xkb.options = "eurosign:e,caps:escape";
-      displayManager.sessionCommands = "setxkbmap -layout fr -variant intl";
+      displayManager.sessionCommands = "setxkbmap -layout us -variant intl";
     };
 
     # OpenSSH server.
