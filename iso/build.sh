f2a71fcb47d6e5ac898182220d2158465900abe5
VideoCurio <david@videocurio.com>
2025-08-24 15:32:57 +0200

    Temp debug

--- iso/build.sh
+++ iso/build.sh
@@ -8,7 +8,8 @@ script_path="$(dirname "$0")"
 branch="$(git branch --show-current)"
 if [[ "$branch" != release* ]]; then
   printf "\e[31m Wrong git branch - not a release!\e[0m\n"
-  exit 1
+  branch="release/25.05.0-RC1" # for debugging only
+  #exit 1
 fi
 currentRelease=$(sed -E "s/release\/(.+)/\1/" <<< "$branch")
 
@@ -18,9 +19,7 @@ nix-build '<nixpkgs/nixos>' --cores 0 --max-jobs auto -A config.system.build.iso
 #sleep 2s
 cp "$script_path"/result/iso/nixos-minimal-*.iso "$script_path"/
 for isofilename in "$script_path"/nixos-minimal-*.iso; do
-  echo "$isofilename"
   newFilename=$(sed -E "s/nixos-minimal-[0-9]{2}\.[0-9]{2}\.(.+)\.iso/nixcosmic-minimal-${currentRelease}.\1.iso/" <<< "$isofilename")
-  echo "$newFilename"
   if [ -f "$newFilename" ]; then
     printf "\e[31m ISO file already exist!\e[0m\n"
     rm -f "$script_path"/result/iso/nixos-minimal-*.iso
