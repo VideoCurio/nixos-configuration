da84baf6b820e34fb65969a07ce23aed180c8caf
VideoCurio <david@videocurio.com>
2025-09-10 11:19:58 +0200

    Install from git clone on RPI4

--- iso/iso-minimal.nix
+++ iso/iso-minimal.nix
@@ -1,5 +1,5 @@
 # CuriOS-minimal ISO configuration file
-# Basic installer for the console. Based on NixOS installation-cd-minimal-new-kernel.nix
+# Basic installer for the console. Based on NixOS ${modulesPath}/installer/cd-dvd/installation-cd-minimal-new-kernel.nix
 # See: https://nixos.wiki/wiki/Creating_a_NixOS_live_CD
 # https://nixos.org/manual/nixos/stable/index.html#sec-building-image
 # https://nixos.org/manual/nixpkgs/stable/#chap-stdenv
