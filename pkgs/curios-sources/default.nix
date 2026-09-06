d2601e17ad092ac3cc7f277fed1531605f467362
VideoCurio <david@videocurio.com>
2026-09-06 17:06:33 +0200

    Fix: herdr install pkgs for ISO image

--- pkgs/curios-sources/default.nix
+++ pkgs/curios-sources/default.nix
@@ -8,7 +8,7 @@
 { lib, stdenvNoCC }:
 stdenvNoCC.mkDerivation {
   pname = "curios-sources";
-  version = "unstable-20260905.0844";
+  version = "unstable-20260906.1337";
 
   src = lib.fileset.toSource {
     root = ../../.;
@@ -46,6 +46,7 @@ stdenvNoCC.mkDerivation {
     install -D -m 644 -t $out/share/curios/pkgs/curios-manager/ pkgs/curios-manager/default.nix
     install -D -m 644 -t $out/share/curios/pkgs/curios-manager-applet/ pkgs/curios-manager-applet/default.nix
     install -D -m 600 -t $out/share/curios/pkgs/curios-manager-applet/ pkgs/curios-manager-applet/Cargo.lock
+    install -D -m 644 -t $out/share/curios/pkgs/herdr/ pkgs/herdr/default.nix
     install -D -m 644 -t $out/share/curios/pkgs/snitch/ pkgs/snitch/default.nix
 
     runHook postInstall
